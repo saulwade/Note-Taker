a5aa3f359b375c2621de3834f904f6ce2d2ba48a
saulwade <saulwade29@gmail.com>
2023-03-01 20:44:50 -0600

    fixed the html bug

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 
 // GET Route for homepage
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
